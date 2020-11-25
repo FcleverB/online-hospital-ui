c0b2ac09680d5135903d2e9758cdc5edd0a852c1
fclecer <1393906015@qq.com>
2020-11-25 08:26:10 +0800

    hospital-system中菜单管理的api编写

---
+++ src/api/system/menu/menu.js
@@ -0,0 +1,52 @@
+import request from '@/utils/request'
+
+// 更新菜单数据
+export function updateMenu(data) {
+  return request({
+    url: '/system/menu/updateMenu',
+    method: 'put',
+    params: data
+  })
+}
+
+// 查询所有菜单数据
+export function listAllMenus(query) {
+  return request({
+    url: '/system/menu/listAllMenus',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询菜单树
+export function selectMenuTree() {
+  return request({
+    url: '/system/menu/selectMenuTree',
+    method: 'get'
+  })
+}
+
+// 根据菜单id删除菜单信息
+export function deleteMenuById(menuId) {
+  return request({
+    url: '/system/menu/deleteMenuById/' + menuId,
+    method: 'delete'
+  })
+}
+
+// 根据菜单id查询指定菜单数据
+export function getMenuById(menuId) {
+  return request({
+    url: '/system/menu/getMenuById/' + menuId,
+    method: 'get'
+  })
+}
+
+// 添加菜单信息
+export function addMenu(data) {
+  return request({
+    url: '/system/menu/addMenu',
+    method: 'post',
+    params: data
+  })
+}
