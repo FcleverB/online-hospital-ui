7355a71a0ee28b4d63ade1d52611680f976391cf
fclecer <1393906015@qq.com>
2020-12-12 12:48:55 +0800

    hospital-erp中生产厂家管理页面增删改查实现

---
+++ src/api/erp/producter/producter.js
@@ -0,0 +1,52 @@
+import request from '@/utils/request'
+
+// 修改生产厂家信息
+export function updateProducter(data) {
+  return request({
+    url: '/erp/producter/updateProducter',
+    method: 'put',
+    params: data
+  })
+}
+
+// 分页查询生产厂家信息
+export function listProducterForPage(query) {
+  return request({
+    url: '/erp/producter/listProducterForPage',
+    method: 'get',
+    params: query
+  })
+}
+
+// 根据id删除生产厂家
+export function deleteProducterByIds(producterIds) {
+  return request({
+    url: '/erp/producter/deleteProducterByIds/' + producterIds,
+    method: 'delete'
+  })
+}
+
+// 根据id查询生产厂家信息
+export function getProducterById(producterId) {
+  return request({
+    url: '/erp/producter/getProducterById/' + producterId,
+    method: 'get'
+  })
+}
+
+// 查询所有生产厂家信息
+export function selectAllProducter() {
+  return request({
+    url: '/erp/producter/selectAllProducter',
+    method: 'get'
+  })
+}
+
+// 添加生产厂家信息
+export function addProducter(data) {
+  return request({
+    url: '/erp/producter/addProducter',
+    method: 'post',
+    params: data
+  })
+}
--- src/router/index.js
+++ src/router/index.js
@@ -210,7 +210,7 @@ export const asyncRoutes = [
     children: [
       {
         path: 'producter',
-        component: () => import('@/views/error-page/404'),
+        component: () => import('@/views/erp/producter/index'),
         name: '/stock/producter',
         meta: { title: '生产厂家管理', icon: 'list' }
       },
