29c35777aea756c26854682ac76f4a67fcf74d70
fclecer <1393906015@qq.com>
2020-12-22 17:13:37 +0800

    hospital-erp中新增采购页面中添加药品数据列表实现

--- src/views/erp/purchase/newPurchase.vue
+++ src/views/erp/purchase/newPurchase.vue
@@ -108,10 +108,96 @@
       </el-table>
     </el-card>
     <!--采购单据详情结束-->
+    <!--药品列表模态框开始-->
+    <el-dialog
+      :title="titleMedicines"
+      :visible.sync="openMedicines"
+      width="1100px"
+      center
+      append-to-body
+      :before-close="closeModal"
+    >
+      <!-- 查询条件开始 -->
+      <el-form ref="queryForm" :model="queryParams" :inline="true" label-width="68px">
+        <el-form-item label="关键字" prop="keywords">
+          <el-input
+            v-model="queryParams.keywords"
+            placeholder="请输入关键字"
+            clearable
+            size="small"
+            style="width:180px"
+          />
+        </el-form-item>
+        <el-form-item label="药品类型" prop="medicinesType">
+          <el-select
+            v-model="queryParams.medicinesType"
+            placeholder="药品类型"
+            clearable
+            size="small"
+            style="width:180px"
+          >
+            <el-option
+              v-for="dict in medicinesTypeOptions"
+              :key="dict.dictValue"
+              :label="dict.dictLabel"
+              :value="dict.dictValue"
+            />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="处方类型" prop="prescriptionType">
+          <el-select
+            v-model="queryParams.prescriptionType"
+            placeholder="处方类型"
+            clearable
+            size="small"
+            style="width:180px"
+          >
+            <el-option
+              v-for="dict in prescriptionTypeOptions"
+              :key="dict.dictValue"
+              :label="dict.dictLabel"
+              :value="dict.dictValue"
+            />
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+          <el-button type="primary" icon="el-icon-refresh" size="mini" @click="resetQuery">清空</el-button>
+        </el-form-item>
+      </el-form>
+      <!-- 查询条件结束 -->
+      <!-- 药品的数据表格开始 -->
+      <el-table ref="medicinesTableList" v-loading="loading" border :data="medicinesTableList" @selection-change="handleSelectionChange">
+        <el-table-column type="selection" width="55" align="center" />
+        <el-table-column label="药品ID" align="center" prop="medicinesId" />
+        <el-table-column label="药品名称" align="center" prop="medicinesName" />
+        <el-table-column label="药品编号" align="center" prop="medicinesNumber" />
+        <el-table-column label="生产厂家" width="280px" align="center" prop="producterId" :formatter="producterFormatter" />
+        <el-table-column label="药品类型" align="center" prop="medicinesType" :formatter="medicinesTypeFormatter" />
+        <el-table-column label="处方类型" align="center" prop="prescriptionType" :formatter="prescriptionTypeFormatter" />
+        <el-table-column label="关键字" align="center" prop="keywords" />
+      </el-table>
+      <!-- 药品的数据表格结束 -->
+      <!-- 分页控件开始 -->
+      <el-pagination
+        v-show="total>0"
+        :current-page="queryParams.pageNum"
+        :page-sizes="[5, 10, 20, 30]"
+        :page-size="queryParams.pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
+      <!-- 分页控件结束 -->
+    </el-dialog>
+    <!--药品列表模态框结束-->
   </div>
 </template>
 
 <script>
+// 引入药品信息api
+import { listMedicinesForPage } from '@/api/erp/medicines/medicines'
 // 引入供应商API
 import { selectAllProvider } from '@/api/erp/provider/provider'
 // 引入生产厂家api
@@ -129,56 +215,41 @@ export default {
   },
   data() {
     return {
+      // 遮罩
+      loading: false,
+      // 是否打开药品的模态框
+      openMedicines: false,
+      // 药品模态框标题
+      titleMedicines: '',
+      // 分页总条数
+      total: '',
       // 是否已经提交
       isSubmit: false,
       // 查询条件供应商数据
       providerOptions: [],
       // 详情列表生产厂家数据
       producterOptions: [],
+      // 药品类型数据
+      medicinesTypeOptions: [],
+      // 处方类型数据
+      prescriptionTypeOptions: [],
       // 入库单据详情列表数据
-      purchaseItemList: [
-        {
-          medicinesId: 1,
-          medicinesName: '白芷',
-          conversion: 1,
-          unit: 'g',
-          producterId: 1,
-          purchaseNumber: 10,
-          tradePrice: 10.00,
-          tradeTotalAmount: 100.00,
-          batchNumber: '1',
-          remark: '备注'
-        },
-        {
-          medicinesId: 2,
-          medicinesName: '白芷2',
-          conversion: 12,
-          unit: 'g',
-          producterId: 2,
-          purchaseNumber: 102,
-          tradePrice: 102.00,
-          tradeTotalAmount: 1002.00,
-          batchNumber: '12',
-          remark: '备注2'
-        },
-        {
-          medicinesId: 3,
-          medicinesName: '白芷3',
-          conversion: 13,
-          unit: 'g',
-          producterId: 3,
-          purchaseNumber: 103,
-          tradePrice: 103.00,
-          tradeTotalAmount: 1003.00,
-          batchNumber: '13',
-          remark: '备注3'
-        }
-      ],
+      purchaseItemList: [],
+      // 药品数据列表信息
+      medicinesTableList: [],
       // 入库单据数据
       form: {
         purchaseId: undefined, // 入库单据号
         providerId: undefined, // 供应商id
         purchaseTradeTotalAmount: undefined // 一张单据的总批发额
+      },
+      // 药品列表查询条件
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+        keywords: undefined,
+        medicinesType: undefined,
+        prescriptionType: undefined
       }
     }
   },
@@ -195,10 +266,32 @@ export default {
     selectAllProducter().then(res => {
       this.producterOptions = res.data
     })
+    // 查询药品类型数据，转换码表
+    this.getDataByType('his_medicines_type').then(res => {
+      // 将查询到的状态信息保存到当前页面对应的属性中
+      this.medicinesTypeOptions = res.data
+    })
+    // 查询处方类型数据，转换码表
+    this.getDataByType('his_prescription_type').then(res => {
+      // 将查询到的状态信息保存到当前页面对应的属性中
+      this.prescriptionTypeOptions = res.data
+    })
   },
   methods: {
+    // 查询数据列表数据
+    getMedicinesList() {
+      this.loading = true
+      listMedicinesForPage(this.queryParams).then(res => {
+        this.medicinesTableList = res.data
+        this.total = res.total
+        this.loading = false
+      })
+    },
     // 添加药品模态框
     handleAddMedicines() {
+      this.openMedicines = true
+      this.titleMedicines = '选择要添加的药品信息'
+      this.getMedicinesList()
     },
     // 批量设置模态框
     handleBatchSet() {
@@ -222,6 +315,47 @@ export default {
         }
       })
       return name
+    },
+    medicinesTypeFormatter(row) {
+      return this.transferDictCode(this.medicinesTypeOptions, row.medicinesType)
+    },
+    prescriptionTypeFormatter(row) {
+      return this.transferDictCode(this.prescriptionTypeOptions, row.prescriptionType)
+    },
+    // 查询操作
+    handleQuery() {
+      // 执行实际的查询方法
+      // 因为输入的查询条件实时与queryParams动态绑定
+      this.getMedicinesList()
+    },
+    // 清空查询条件操作
+    resetQuery() {
+      // 清空查询数据
+      this.resetForm('queryForm')
+      // 重新查询数据列表,相当于执行一次无查询条件的查询操作,如果不调用这个方法,那么清空操作后,数据列表不会同步改变
+      this.getMedicinesList()
+    },
+    // 改变数据列表第一列多选框选中状态所触发的方法,selection为选择的内容
+    handleSelectionChange(selection) {
+    },
+    // 改变每页显示条数的时候触发
+    handleSizeChange(val) {
+      // 更新每页显示条数
+      this.queryParams.pageSize = val
+      // 重新查询
+      this.getMedicinesList()
+    },
+    // 当前页改变时触发(前一页,点击某一页,下一页,跳转某一页)
+    handleCurrentChange(val) {
+      // 更新需要显示的第几页数
+      this.queryParams.pageNum = val
+      // 重新查询
+      this.getMedicinesList()
+    },
+    // 关闭药品模态框需要清空数据
+    closeModal(done) {
+      this.resetQuery()
+      done()
     }
   }
 }
